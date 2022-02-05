1664d6d5a714586c160f17028de79bf681d4d2d4
soon <soon_myeong@naver.com>
2022-02-06 00:29:29 +0900

    과거 개발자들이 했던 FutureTask 구현

--- src/test/java/chap04/FutureEx.java
+++ src/test/java/chap04/FutureEx.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Objects;
+import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
@@ -38,5 +36,60 @@ public class FutureEx {
           Hello
          */
     }
+
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    interface ExceptionCallback {
+        void onError(Throwable t);
+    }
+
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+
+        SuccessCallback sc;
+        ExceptionCallback ec;
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc, ExceptionCallback ec) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc); // if(sc == null) 대신 심플하게 Objects 유틸 메서드 사용
+            this.ec = Objects.requireNonNull(ec);
+        }
+
+        @Override
+        protected void done() {
+            try {
+                sc.onSuccess(get());
+            } catch (InterruptedException e) { //해당 exception 의 경우, 신호를 던져주는게 중요함,
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                ec.onError(e.getCause());
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("FutureTask 만들어보기")
+    public void make_FutureTask() {
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        CallbackFutureTask f = new CallbackFutureTask(()-> {
+            Thread.sleep(2000);
+            if(1==1) throw new RuntimeException("Async Error!"); //일부로 예외 만들어 보기
+            log.info("Async");
+            return "Hello";
+        }, System.out::println, e-> System.out.println("Error : " + e.getMessage()));
+
+        es.execute(f);
+
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
